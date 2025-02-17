d91b02b85ab669a38e0b2a3671e906d66c658536
NoodleMX <abel.bd96@gmail.com>
2025-02-17 12:28:39 -0600

    Add Mokepon selection functionality and update HTML structure

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function selectPlayerMokepon() {
+  alert("You have selected a Mokepon!");
+}
+
+const selectMokeponButton = document.getElementById("select-mokepon-button");
+selectMokeponButton.addEventListener("click", selectPlayerMokepon);
