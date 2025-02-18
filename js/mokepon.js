d4e5d0591f031acfc8d934c8267f941201b79970
NoodleMX <abel.bd96@gmail.com>
2025-02-18 12:22:50 -0600

    Add enemy Mokepon selection and update life count display

--- js/mokepon.js
+++ js/mokepon.js
@@ -18,6 +18,25 @@ function selectPlayerMokepon() {
   } else {
     alert("Please select a Mokepon!");
   }
+  selectEnemyMokepon();
+}
+
+function selectEnemyMokepon() {
+  let enemyMokepon = generateRandomNumber(1, 3);
+  let spanEnemyMokepon = document.getElementById("enemy-mokepon");
+
+  if (enemyMokepon === 1) {
+    spanEnemyMokepon.innerHTML = "Charmander";
+  } else if (enemyMokepon === 2) {
+    spanEnemyMokepon.innerHTML = "Squirtle";
+  } else if (enemyMokepon === 3) {
+    spanEnemyMokepon.innerHTML = "Bulbasaur";
+  }
+}
+
+// Function to generate a random number between min and max
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 window.addEventListener("load", startGame);
