e1695f079d53dc2699cedc57fa8961720e4aa940
NoodleMX <abel.bd96@gmail.com>
2025-02-18 10:25:35 -0600

    Initialize Mokepon selection functionality and update event handling

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,22 @@
-function selectPlayerMokepon() {
-  alert("You have selected a Mokepon!");
+function startGame() {
+  const selectMokeponButton = document.getElementById("select-mokepon-button");
+  selectMokeponButton.addEventListener("click", selectPlayerMokepon);
 }
 
-const selectMokeponButton = document.getElementById("select-mokepon-button");
-selectMokeponButton.addEventListener("click", selectPlayerMokepon);
+function selectPlayerMokepon() {
+  let inputCharmander = document.getElementById("charmander");
+  let inputSquirtle = document.getElementById("squirtle");
+  let inputBulbasaur = document.getElementById("bulbasaur");
+
+  if (inputCharmander.checked) {
+    alert("You selected Charmander!");
+  } else if (inputSquirtle.checked) {
+    alert("You selected Squirtle!");
+  } else if (inputBulbasaur.checked) {
+    alert("You selected Bulbasaur!");
+  } else {
+    alert("Please select a Mokepon!");
+  }
+}
+
+window.addEventListener("load", startGame);
